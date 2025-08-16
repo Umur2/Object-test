ed7c9cad9fde04f6672d21659369eafbeb22199b
Umur2 <fusunaltintas@gmail.com>
2025-08-16 12:12:34 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+# calculator.py dosyasındaki Calculator sınıfını içe aktar
+from calculator import Calculator
+# Calculator sınıfından bir nesne oluştur
+Calcy = Calculator()
+
+# Toplama işlemi yap ve sonucu yazdır
+result=Calcy.add(7,100)
+print(result)
+# Çıkarma işlemi yap ve sonucu yazdır
+result = Calcy.subtract(10,5)
+print(result)
+
+# Çarpma işlemi yap ve sonucu yazdır
+
+
+# Bölme işlemi yap ve sonucu yazdır
+
+
+# Sıfıra bölme denemesi yap ve sonucu yazdır (hata mesajı döner)
+
